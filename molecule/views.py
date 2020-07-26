d70708943021631e1b27c79d26460ffcbbf213c7
rtviii <rtkushner@gmail.com>
2020-07-26 22:43:57 +0000

    f

--- molecule/views.py
+++ molecule/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets          # add this
+from rest_framework import viewsets          
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import     MoleculeSerializer
 from .models import Molecule     
-
-# Create your views here.
+from neo4j import GraphDatabase
 
 
 
@@ -11,3 +12,33 @@ from .models import Molecule
 class MoleculeView(viewsets.ModelViewSet):
     serializer_class = MoleculeSerializer
     queryset = Molecule.objects.all()
+
+
+uri = "neo4j://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("rt", "rrr"))
+
+
+
+
+
+@api_view(['GET', 'POST'])
+def get_molecule(request):
+    print(request.GET)
+    params = dict(request.GET)
+    identifier = params['identifier']
+    print("IDENTIFIER IS " ,identifier)
+    CYPHER_STRING="""match (S:Subchain {nomenclature:%s})-[]-(pf:PfamFamily)
+                with S, S.nomenclature as noms, pf
+                return *;"""%identifier
+
+    if request.method == 'GET':
+        def get_molecules(tx):
+            molecules = []
+            return list(tx.run(CYPHER_STRING))
+            
+        with driver.session(database='ribosome') as session:
+            result = session.read_transaction(get_molecules)
+        driver.close()
+        return Response(result)
+
+
