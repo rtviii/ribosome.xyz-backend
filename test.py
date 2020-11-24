9fe6d91bf832a0effd67bb9945cf3bc7314032f6
rtviii <rtkushner@gmail.com>
2020-11-23 20:08:01 -0800

    migrated to 4.2.0

--- test.py
+++
@@ -1,8 +0,0 @@
-def dd(**kwargs):
-    for b in kwargs.items():
-        print(b[0], b[1])
-    # for k in kwargs.keys():
-    #     print(k)
-    # for s in kwargs.values():
-    #     print(s)
-    
