77fc90a3567ff9f44881942c28bf064873c6ac7f
rtviii <rtkushner@gmail.com>
2021-09-06 13:27:01 -0700

    Before integrating into the backend

--- static_files/align.py
+++ static_files/align.py
@@ -15,9 +15,12 @@ TEMP_CHAIN = os.getenv("TEMP_CHAIN")
 
 cmd.load (handle1    ,pymol_name1)
 cmd.load (handle2    ,pymol_name2)
-cmd.align(pymol_name1,pymol_name2)
+cmd.super(pymol_name1,pymol_name2)
 
-cmd.save(TEMP_CHAIN,state=1,partial=1)
+print("Aligning using SUPER")
 
+cmd.save(TEMP_CHAIN)
+
+print(f"Saved to {TEMP_CHAIN}")
 
 
