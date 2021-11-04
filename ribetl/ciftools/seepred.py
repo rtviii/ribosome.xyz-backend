8be90422f191d0c28cfe954f6192609d3e76aa4f
rtviii <rtkushner@gmail.com>
2021-11-03 17:26:25 -0700

    before extending align3d endpoint with ranged param

--- ribetl/ciftools/seepred.py
+++ ribetl/ciftools/seepred.py
@@ -7,6 +7,7 @@ from pymol import cmd
 import argparse
 sys.path.append('/home/rxz/dev/riboxyzbackend')
 import ribetl.ciftools.transpose_ligand as transpose_ligand
+from ribetl.ciftools.super import ranged_super
 import ribetl.ciftools.bsite_mixed as bsite_mixed
 sys.path.append('../../')
 
@@ -126,6 +127,27 @@ def see_poly(POLY,SRC):
 
 		
 
+
+@cmd.extend
+def rsuper(src_struct,tgt_struct,poly_class,rstart,rend):
+	# cmd.delete('all')
+
+	[ c1 ,r1,c2,r2 ] = ranged_super( poly_class   ,src_struct, tgt_struct,( int(rstart), int(rend )))
+
+	n1 = c1.split("/")[-1].split('.')[0]
+	n2 = c2.split("/")[-1].split('.')[0]
+
+	cmd.load(c1)
+	cmd.select("resi {}-{} and m. {} ".format(*r1,n1))
+	cmd.create("snip1","sele")
+	cmd.delete(n1)
+
+	cmd.load(c2)
+	cmd.select("resi {}-{} and m. {} ".format(*r2,n2))
+	cmd.create("snip2","sele")
+	cmd.delete(n2)
+	cmd.super("snip1","snip2")
+	cmd.save("RANGED_ALIGNMENT.pdb")
 
 
 		
