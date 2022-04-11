e2b9e4fe8ce719d8fa22a3ecdcb63300f28c7b88
Artem <rtkushner@gmail.com>
2022-04-10 19:57:36 -0700

    found how to add ete3 child nodes

--- ribetl/taxonomy/taxonomy_fig.py
+++ ribetl/taxonomy/taxonomy_fig.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import json
 from lib2to3.pytree import Node
 import os
+from pprint import pprint
 from typing import List
 import dotenv
 import numpy as np
@@ -9,9 +10,13 @@ import pandas as pd
 from ete3 import NCBITaxa,  TreeStyle,  faces, AttrFace, TextFace, NodeStyle, CircleFace, ImgFace, RectFace,SeqMotifFace
 from generate_taxtree import TaxaProfile, profile_taxa
 
-BACTERIA        = 2
-ARCHAEA         = 2157
-EUKARYA         = 2759
+BACTERIA = 2
+ARCH  = 2157
+EUKARYA  = 2759
+
+# BACTERIA_other        = 9992
+# ARCHAEA_other        = 9992157
+# EUKARYA_other        = 9992759
 
 dotenv.load_dotenv(dotenv_path='/home/rxz/dev/riboxyzbackend/rxz_backend/.env')
 STATIC_ROOT = os.environ.get('STATIC_ROOT')
@@ -21,15 +26,24 @@ profiles      = list(map(lambda _: os.path.join(STATIC_ROOT,_,f"{_}.json"),struc
 org_id_arrays = []
 ncbi          = NCBITaxa()
 
+
+
+MAIN_IDS=['S. oleracea', 'T. brucei', 'S. scrofa', 'H. sapiens', 'O. cuniculus', 'K. lactis', 'S. cerevisiae', 'C. thermophilum', 'E. coli', 
+        'A. baumanii', 'P. aeruginosa', 'B. subtilis', 'F. faecalis', 'T. thermophilus', 'D. radiodurans','M. smegmatis', 'S. aureus']
+
+
 def node_lineage(node):
-    return  ncbi.get_lineage( node.taxid )
+    # print(node.taxid," is lineage ", ncbi.get_lineage(node.taxid ));
+    return ncbi.get_lineage(node.taxid )
+
+def taxid_to_linnaean(spec_taxid:int):_ = str(ncbi.get_taxid_translator([spec_taxid])[spec_taxid]).split(" "); return _[0][0] + ". "+ _[1]
 
 def lift_rank(taxid:int)->int:
-
 	"""Given a taxid, make sure that it's a SPECIES (as opposed to strain, subspecies, isolate, norank etc.)"""
-	if ncbi.get_rank([taxid])[taxid] == 'species':
 
+	if ncbi.get_rank([taxid])[taxid] == 'species':
 		return taxid
+
 	else:
 		lin = iter(ncbi.get_lineage(taxid))
 		node = 1
@@ -37,107 +51,123 @@ def lift_rank(taxid:int)->int:
 			node = next(lin)
 		return node
 		
+def tail_species_to_other(taxid:int ):
+	...
 	
 taxprofiles:List[TaxaProfile] = list(map(profile_taxa, profiles))
-taxons      = list(map( lift_rank, map(lambda _: _.classified_as, taxprofiles)))
-ncbi        = NCBITaxa()
-t           = ncbi.get_topology(taxons)
 
-def taxid_to_linnaean(spec_taxid:int):_ = str(ncbi.get_taxid_translator([spec_taxid])[spec_taxid]).split(" "); return _[0][0] + ". "+ _[1]
+taxons      = list(map( lift_rank, map(lambda _: _.classified_as, taxprofiles)))
+
+ncbi          = NCBITaxa()
+ncbi_topology = ncbi.get_topology(taxons)
+# print("gto topoloyg", ncbi_topology)
+
+
+
+print("----------------------++")
+
+# def node_get_upto_kingdom(node):
+	# ncbi    = NCBITaxa()
+	# nodeid  = node.taxid;
+	# lineage = list( ncbi.get_lineage(nodeid) );
+	# print("Got lineage", lineage)
+	# if len(lineage) < 3:
+	# 	return node
+	# kingdom = lineage[2]
+
+	# if kingdom == BACTERIA:
+	# 	print("adding bacteria other", BACTERIA_other)
+	# 	lineage_extended = [*lineage, BACTERIA_other]
+	# 	node.taxid = BACTERIA_other
+	# if kingdom == EUKARYA:
+	# 	print("adding bacteria other", EUKARYA_other)
+	# 	lineage_extended = [*lineage, EUKARYA_other]
+	# 	node.taxid = EUKARYA_other
+	# if kingdom == ARCHAEA:
+	# 	print("adding bacteria other", ARCHAEA_other)
+	# 	lineage_extended = [*lineage, ARCHAEA_other]
+	# 	node.taxid = ARCHAEA_other
+
+
+		
+	# return node 
+     
+
+	
+
+
+ 
+
+def find_main_nodes(node):
+	nodeid = node.taxid
+	lineage = ncbi.get_lineage(nodeid)
+	names   = ncbi.translate_to_names(lineage)
+
+	for ( tid,n ) in zip(lineage,names):
+		if list( ncbi.get_rank([ tid ]).values() )[0] == 'species':
+			linaen = taxid_to_linnaean(tid)
+			print(linaen, tid)
+			if linaen in MAIN_IDS:
+				print("Detected main node ", node.taxid)
+				...
+			else:
+				...
+
+       
+		
+
+# for i in [1, 131567, 2759, 33154, 33208, 6072, 33213, 33511, 7711, 89593, 7742, 7776, 117570, 117571, 8287, 1338369, 32523, 32524, 40674, 32525, 9347, 1437010, 314146, 314147, 9989, 1963758, 337687, 10066, 39107, 10088, 862507, 10090]:
+#     print(ncbi.get_rank([ i ]))
+#     tid_ = list(ncbi.get_rank([ i ]).keys())
+#     print(ncbi.get_common_names(tid_))
+#     print()
+print("----------------------++")
+
+
+
 def layout(n):
-	tid = n.taxid
+
+	if 'manual' in n.name:
+
+		taxnamenode = TextFace( 
+			f"Manual X" ,
+			fsize    = 16,
+			penwidth = 2,
+			fstyle   = 'italic',
+			fgcolor  = 'black' )
+
+		taxnamenode.margin_bottom = 80
+		taxnamenode.rotation      = -50
+		faces.add_face_to_node(taxnamenode, n,column=2 , aligned=True)
+
+		textface_node               = TextFace(f"COUHNt" , fsize = 16, fgcolor= 'black' )
+		textface_node.rotation      = -90
+		textface_node.margin_top    = 10
+		textface_node.margin_bottom = 14
+		faces.add_face_to_node(textface_node, n,    column   =1, aligned=True)
+		return
+	tid   = n.taxid
 	count = 0
 	for s in taxons:
 		if s == tid: count+=1
 
 	if tid in taxons:
-
-
-
-		def getcolor(c):
-			if  0 > c > 10:
-				return "BlanchedAlmond"
-			if  10 > c > 25:
-				return "NavajoWhite"
-			if  25 > c > 50:
-				return "BurlyWood"
-			if  50 > c > 100:
-				return "SandyBrown"
-			if  100 > c > 150:
-				return "Sienna"
-			if   c > 150:
-				return "Maroon"
-
-		C = CircleFace(radius= 4 +  10 * count/250, color=getcolor(count), style="circle")
-		C.opacity = 0.6
-		C.margin_top         = 5
-		C.margin_right       = 5
-		C.margin_left        = 5
-		C.margin_bottom      = 5
-		
-		ligands_svg = '/home/rxz/dev/riboxyzbackend/ribetl/taxonomy/ligand_icon.svg'
-		# imgf= ImgFace(ligands_svg, 30,30)
-
-		rna_rect                      = TextFace("87", fsize=8, fgcolor='PaleTurquoise')
-		factors_rect                  = TextFace("25", fsize=8, fgcolor='PaleTurquoise')
-
-		rna_rect.margin_top           = 5
-		rna_rect.margin_right         = 5
-		rna_rect.margin_left          = 5
-		rna_rect.margin_bottom        = 5
-		rna_rect.opacity              = 0.9
-		rna_rect.border.width         = 1
-		rna_rect.background.color     = "SteelBlue"
-		factors_rect.margin_top       = 5
-		factors_rect.margin_right     = 5
-		factors_rect.margin_left      = 5
-		factors_rect.margin_bottom    = 5
-		factors_rect.opacity          = 0.9
-		factors_rect.border.width     = 1
-		factors_rect.background.color = "DimGray"
-		rna_rect.hz_align             = 1
-		factors_rect.hz_align         = 3
-
-		
-		
-		# seq = ("-----------------------------------------------AQAK---IKGSKKAIKVFSSA---"
-		# "APERLQEYGSIFTDA---GLQRRPRHRIQSK-------ALQEKLKDFPVCVSTKPEPEDDAEEGLGGLPSN"
-		# "ISSVSSLLLFNTTENLYKKYVFLDPLAG----THVMLGAETEEKLFDAPLSISKREQLEQQVPENYFYVPD"
-		# "LGQVPEIDVPSYLPDLPGIANDLMYIADLGPGIAPSAPGTIPELPTFHTEVAEPLKVGELGSGMGAGPGTP"
-		# "AHTPSSLDTPHFVFQTYKMGAPPLPPSTAAPVGQGARQDDSSSSASPSVQGAPREVVDPSGGWATLLESIR"
-		# "QAGGIGKAKLRSMKERKLEKQQQKEQEQVRATSQGGHL--MSDLFNKLVMRRKGISGKGPGAGDGPGGAFA"
-		# "RVSDSIPPLPPPQQPQAEDED----")
-
-
-		# seqFace = SeqMotifFace(seq, gapcolor="blue")
-		# seqFace = SeqMotifFace(seq[40:100], seq_format="seq")
-
 		taxnamenode = TextFace( 
 			f"{taxid_to_linnaean(tid)}" ,
-			fsize    = 14,
+			fsize    = 16,
 			penwidth = 2,
 			fstyle   = 'italic',
 			fgcolor  = 'black' )
 
-
-		taxnamenode.margin_top    = 20
-		taxnamenode.margin_bottom = 10
-		taxnamenode.margin_left   = 10
-		taxnamenode.margin_right  = 10
+		taxnamenode.margin_bottom = 80
+		taxnamenode.rotation      = -50
 		faces.add_face_to_node(taxnamenode, n,column=2 , aligned=True)
 
 		textface_node               = TextFace(f"{count}" , fsize = 16, fgcolor= 'black' )
-		textface_node.rotation      = 90
+		textface_node.rotation      = -90
 		textface_node.margin_top    = 10
 		textface_node.margin_bottom = 14
-		# textface_node.margin_left   = 10
-		# textface_node.margin_right  = 10
 		faces.add_face_to_node(textface_node, n,    column   =1, aligned=True)
-
-		# faces.add_face_to_node(C                         ,                                 n, 2, position ="float", aligned=True)
-		# faces.add_face_to_node(rna_rect                  ,                                 n, 3, position ="float", aligned=True)
-		# faces.add_face_to_node(factors_rect              ,                                 n, 4, position ="float", aligned=True)
-		# faces.add_face_to_node(seqFace              ,                                 n, 2, position ="float", aligned=True)
 
 ts                     = TreeStyle()
 ts.mode                = "r"
@@ -147,26 +177,31 @@ ts.show_branch_support = False
 ts.show_scale          = False
 ts.show_leaf_name      = False
 ts.draw_guiding_lines  = True
-ts.rotation            = 270
-ts.rotation            = -90
+ts.rotation            = -270
+# ts.rotation            = -90
 
-for n in t.traverse():
+ncbi_topology.search_nodes(taxid=BACTERIA)[0].add_child(name='Bac_manual')
+ncbi_topology.search_nodes(taxid=EUKARYA)[0].add_child(name='Euk_manual')
+ncbi_topology.search_nodes(taxid=131567)[0].add_child(name='Arch_manual')
+
+for n in ncbi_topology.traverse():
+	# find_main_nodes(n)
 	nstyle = NodeStyle()
-
+	
+	if 'manual' in n.name:
+		continue
 	if BACTERIA in node_lineage(n):
-		# nstyle["bgcolor"]              = "PaleGreen"
-		nstyle["bgcolor"]              ="#DCFCDE"
-	if ARCHAEA in node_lineage(n):
-		# nstyle["bgcolor"]              = "PowderBlue"
-		nstyle["bgcolor"]              = "#DDF2FA"
+		nstyle["bgcolor"] ="#DCFCDE"
 	if EUKARYA in node_lineage(n):
-		# nstyle["bgcolor"] = "OldLace"
 		nstyle["bgcolor"] = "#FAF8DD"
+	if ARCH in node_lineage(n):
+		nstyle["bgcolor"] = "#DDF2FA"
+     
+	# n = node_get_upto_kingdom(n)
 
-	#      nstyle['shape'] = 'circle'
-	nstyle["shape"]        = "square"
-	nstyle["size"]         = 0
-	nstyle["fgcolor"]      = "blue"
+	nstyle["shape"]   = "square"
+	nstyle["size"]    = 0
+	nstyle["fgcolor"] = "blue"
 
 
 	# nstyle["vt_line_color"] = 10
@@ -176,8 +211,21 @@ for n in t.traverse():
 
 	nstyle["fgcolor"]       = "blue"
 	nstyle["fgcolor"]       = "blue"
+
+
+	print(n.taxid)
+	print(ncbi.get_lineage(n.taxid))
 	n.set_style(nstyle)
 
 
-t.show(tree_style=ts)
-# t.render("tree_flipped.svg", tree_style=ts)
+
+# print(ncbi_topology.search_nodes(taxid=ARCH))
+
+# ncbi_topology.search_nodes(taxid=BACTERIA)[0].add_child(name='OtherB')
+# ncbi_topology.search_nodes(taxid=EUKARYA)[0].add_child(name='OtherE')
+# ncbi_topology.search_nodes(taxid=ARCHAEA)[0].add_child(name='OtherA')
+
+
+
+ncbi_topology.show(tree_style=ts)
+# ncbi_topology.render("tree_flipped.svg", tree_style=ts)
