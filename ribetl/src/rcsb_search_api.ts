80ca9546f7ece694e2110028df97de5da51e4268
Artem <rtkushner@gmail.com>
2022-07-21 08:33:57 -0700

    unpacking gzips

--- ribetl/src/rcsb_search_api.ts
+++ ribetl/src/rcsb_search_api.ts
@@ -1,62 +1,104 @@
 // https://search.rcsb.org/#introduction
 import axios from "axios";
+import { gzip, ungzip } from 'node-gzip'
+import fs from 'fs'
+var http = require('http');
+import { createGzip, unzip } from 'zlib'
 
-var rcsb_search_api = "https://search.rcsb.org/rcsbsearch/v2/query"
-const params = {
-    "query":
-    {
-        "type": "group",
-        "logical_operator": "and",
-        "nodes": [
-            {
-                "type": "group",
-                "logical_operator": "and",
-                "nodes": [
-                    { "type": "group", "logical_operator": "and", "nodes": [{ "type": "terminal", "service": "text", "parameters": { "operator": "contains_phrase", "negation": false, "value": "RIBOSOME", "attribute": "struct_keywords.pdbx_keywords" } }] },
-                    { "type": "group", "logical_operator": "and", "nodes": [{ "type": "terminal", "service": "text", "parameters": { "operator": "greater", "negation": false, "value": 25, "attribute": "rcsb_entry_info.polymer_entity_count_protein" } }] },
-                    { "type": "group", "logical_operator": "and", "nodes": [{ "type": "terminal", "service": "text", "parameters": { "operator": "less", "negation": false, "value": 4, "attribute": "rcsb_entry_info.resolution_combined" } }] }
-                ],
-                "label": "text"
-            }
-        ],
-        "label": "query-builder"
-    },
-    "return_type": "entry",
-    "request_options": {
-        "return_all_hits": true,
-        "results_verbosity": "compact"
+
+
+
+
+
+const missing_structures = async () => {
+    console.log("Getting missing structures");
+    var rcsb_search_api = "https://search.rcsb.org/rcsbsearch/v2/query"
+    const params = {
+        "query":
+        {
+            "type": "group",
+            "logical_operator": "and",
+            "nodes": [
+                {
+                    "type": "group",
+                    "logical_operator": "and",
+                    "nodes": [
+                        { "type": "group", "logical_operator": "and", "nodes": [{ "type": "terminal", "service": "text", "parameters": { "operator": "contains_phrase", "negation": false, "value": "RIBOSOME", "attribute": "struct_keywords.pdbx_keywords" } }] },
+                        { "type": "group", "logical_operator": "and", "nodes": [{ "type": "terminal", "service": "text", "parameters": { "operator": "greater", "negation": false, "value": 25, "attribute": "rcsb_entry_info.polymer_entity_count_protein" } }] },
+                        { "type": "group", "logical_operator": "and", "nodes": [{ "type": "terminal", "service": "text", "parameters": { "operator": "less", "negation": false, "value": 4, "attribute": "rcsb_entry_info.resolution_combined" } }] }
+                    ],
+                    "label": "text"
+                }
+            ],
+            "label": "query-builder"
+        },
+        "return_type": "entry",
+        "request_options": {
+            "return_all_hits": true,
+            "results_verbosity": "compact"
+        }
+    };
+    let query = rcsb_search_api + "?json=" + encodeURIComponent(JSON.stringify(params))
+    let cypherstring = "match (struct:RibosomeStructure) return struct.rcsb_id"
+    cypherstring = encodeURIComponent(cypherstring);
+
+    let ribxz_query = `http://localhost:8000/neo4j/cypher/?cypher=${cypherstring}`
+
+    return Promise.all([axios.get(ribxz_query), axios.get(query)]).then(r => {
+        var ribxz_structs: string[] = r[0].data
+        var rcsb_structs: string[] = r[1].data.result_set
+
+        var missing_from_ribxz = rcsb_structs.filter(struct => {
+            if (!ribxz_structs.includes(struct)) {
+                return true
+            } else return false
+        })
+        console.log(`riboxyz contains ${ribxz_structs.length} structures. Up-to-date RCSB API contains ${rcsb_structs.length} structures.`)
+        console.log("Structs absent from ribosome.xyz: ", missing_from_ribxz.length)
+        return missing_from_ribxz
+    }).catch(e => { console.log(`Rejected : ${e}`); return [] })
+}
+
+const place_unpack_downloads = async (rcsb_structs_to_download: string[]) => {
+
+
+    const BASE_URL = "http://files.rcsb.org/download/"
+    const FORMAT = ".cif.gz"
+
+    for (var structid of rcsb_structs_to_download.slice(0, 5)) {
+        let url = BASE_URL + structid.toUpperCase() + FORMAT
+        let compressed:Buffer = await axios.get(url, {responseType:'arraybuffer'}).then(r => {return r.data})
+            .catch(e => {console.log(`Structure ${structid} failed: `, e); return [];})
+
+        let decompressed = await  ungzip(compressed);
+        fs.writeFileSync(`./${structid}.cif`, decompressed)
+
+        // let _ = unzip(compressed, (err: any, data) => { console.log("decompressed data", data); fs.writeFileSync(`./${structid}.cif`, data.toString()) });
+
+
     }
-};
-let query = rcsb_search_api + "?json=" + encodeURIComponent(JSON.stringify(params))
-// axios.get(query).then(r => console.log(r.data.result_set)).catch(e => console.log(e))
+    // let gzips = await Promise.all(pending_downloads);
+    // console.log(`Got gzips : ${gzips}`)
+    // let uncompressed = await Promise.all(gzips.map(async gz => await ungzip(gz)) as Promise<Buffer>[])
 
-let cypherstring  = "match (struct:RibosomeStructure) return struct.rcsb_id"
-    cypherstring  = encodeURIComponent(cypherstring);
+    // uncompressed.forEach( (v)=>{
 
-let ribxz_query   = `http://localhost:8000/neo4j/cypher/?cypher=${cypherstring}`
-
-Promise.all([axios.get(ribxz_query),axios.get(query)]).then(r => { 
-    var ribxz_structs:string[] = r[0].data
-    var rcsb_structs:string[]  = r[1].data.result_set
-    var intersection           = rcsb_structs.filter(e => !ribxz_structs.includes(e))
-    console.log("Structs absent from ribosome.xyz: ", intersection)
-}).catch(e=>{
-        console.log("Failed to fetch." ,e)
-    })
+    // }
+    // )
 
 
 
+    // return fs.writeFile(`./${structid}.cif`, await ungzip(r.data), function (err) {
+    //     if (err) return console.log(err);
+    //     console.log(`written to file ${structid}`);
+    // });
+}
 
 
 
+const update_ribosome_xyz = async () => {
+    await place_unpack_downloads(await missing_structures())
+}
 
 
-
-
-// axios.get(search_api, {
-//     params,
-//     paramsSerializer: (p: any) => qs.stringify(p, { encode: true })
-// }).then(r => console.log(r.data)).catch(e => { console.log(`Could fetch it. Err: ${e}`) })
-
-// axios.get("https://search.rcsb.org/rcsbsearch/v2/query?json=%7B%22query%22%3A%7B%22type%22%3A%22group%22%2C%22logical_operator%22%3A%22and%22%2C%22nodes%22%3A%5B%7B%22type%22%3A%22terminal%22%2C%22service%22%3A%22full_text%22%2C%22parameters%22%3A%7B%22value%22%3A%22%5C%22thymidine%20kinase%5C%22%22%7D%7D%2C%7B%22type%22%3A%22terminal%22%2C%22service%22%3A%22text%22%2C%22parameters%22%3A%7B%22operator%22%3A%22exact_match%22%2C%22value%22%3A%22Viruses%22%2C%22attribute%22%3A%22rcsb_entity_source_organism.taxonomy_lineage.name%22%7D%7D%2C%7B%22type%22%3A%22terminal%22%2C%22service%22%3A%22text%22%2C%22parameters%22%3A%7B%22operator%22%3A%22exact_match%22%2C%22value%22%3A%22X-RAY%20DIFFRACTION%22%2C%22attribute%22%3A%22exptl.method%22%7D%7D%2C%7B%22type%22%3A%22terminal%22%2C%22service%22%3A%22text%22%2C%22parameters%22%3A%7B%22operator%22%3A%22less_or_equal%22%2C%22value%22%3A2.5%2C%22attribute%22%3A%22rcsb_entry_info.resolution_combined%22%7D%7D%2C%7B%22type%22%3A%22terminal%22%2C%22service%22%3A%22text%22%2C%22parameters%22%3A%7B%22operator%22%3A%22greater%22%2C%22attribute%22%3A%22rcsb_entry_info.nonpolymer_entity_count%22%2C%22value%22%3A0%7D%7D%5D%7D%2C%22return_type%22%3A%22entry%22%7D").then(r=>console.log(r.data)).catch(e=>{console.log(`Could fetch it. Err: ${e}`)})
-
+update_ribosome_xyz().then()
