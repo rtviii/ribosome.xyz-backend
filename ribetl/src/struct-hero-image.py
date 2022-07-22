7d752c3dd3c9ec82beadab7b6534a25cd6fe72ef
Artem <rtkushner@gmail.com>
2022-07-22 13:42:13 -0700

    created a new hero image file in src. preparing to deprecate 'ciftools'

--- ribetl/src/struct-hero-image.py
+++ ribetl/src/struct-hero-image.py
@@ -7,9 +7,9 @@ from pymol import cmd, util
 
 parser = argparse. ArgumentParser(description='generate a ribosome image')
 
-parser. add_argument ("-s"    , "--structure"          , type= str , help="RCSB ID of structure to process"                               )
-parser. add_argument ("-env"  , "--dotenv_path"        , type= str , help="Fallback dotenv path. Needed to locate the static files folder")
-parser. add_argument ("-dest" , "--react_public_folder", type= str , help="Static folder for react app, from where the images are ultimately served.")
+parser. add_argument ("-s"    , "--structure"          , type= str ,required=True, help="RCSB ID of structure to process"                               )
+parser. add_argument ("-env"  , "--dotenv_path"        , type= str ,required=True, help="Fallback dotenv path. Needed to locate the static files folder")
+parser. add_argument ("-dest" , "--react_public_folder", type= str ,required=True, help="Static folder for react app, from where the images are ultimately served.")
 # destination = f"/home/rxz/dev/ribosome.xyz-frontend.ts/public/ray_templates/_ray_{pdbid}.png"
 
 args  = parser.parse_args()
