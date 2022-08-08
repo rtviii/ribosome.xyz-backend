690d03e4b97057137a5ca61c92b0bc5e3e33a6a8
Artem <rtkushner@gmail.com>
2022-08-08 13:36:34 -0700

    deposit_date to initial_release_date

--- ribetl/review_barplot.py
+++ ribetl/review_barplot.py
@@ -6,7 +6,7 @@ from matplotlib import rc, rcParams
 
 
 # open the structs.json file and read the data 
-with open('structs.json', 'r') as infile:
+with open('structs_date.json', 'r') as infile:
     data = json.load(infile)
 
 dates   = []
@@ -20,43 +20,41 @@ by_year  = {
     }
 }
 
-
-nmr  = 0
-em   = 0
-xray = 0
-
-
 XRAY = 'X-RAY DIFFRACTION'
 NMR  = 'SOLUTION NMR'
 EM   = 'ELECTRON MICROSCOPY'
 
 
 for s in data:
-    _                += 1
-    datestr           = str(s['data']['rcsb_accession_info']['deposit_date']).split("T")[0]
+    datestr           = str(s['data']['rcsb_accession_info']['initial_release_date']).split("T")[0]
+    print("datestr is ", datestr)
     method            = str(s['data']['exptl'][0]['method']);
-    reso = s['data']['rcsb_entry_info']['resolution_combined']
+    reso              = s['data']['rcsb_entry_info']['resolution_combined']
     [year,month,day]  = datestr.split("-")
-    if reso == None or int( reso[0] ) >4:
-        print("fitlered out ", s)
-        continue
+
+    # if reso == None or int( reso[0] ) >4:
+    #     print("fitlered out ", s)
+    #     continue
+
     if int(year) < 2000:
         continue
     d                 = datetime.datetime(int(year),int(month),int(day))
+
     print(reso)
     if year not in by_year:
         by_year[year] = {
             'xray': 0,
             'em'  : 0
         }
+
     if method == XRAY:
         by_year[year]['xray'] += 1
-        xray +=1
+
     elif method == NMR:
-        nmr +=1 
+        ...
     elif method == EM:
         by_year[year]['em'] += 1
-        em+=1
+
     dates.append(d)
     heights.append(_)
     
@@ -77,9 +75,8 @@ for e in by_year:
 width         = 0.35       # the width of the bars: can also be len(x) sequence
 fig, ax = plt.subplots()
 rc('axes', linewidth=2)
-# rc('font', weight='bold')
-xraybars = ax.bar(d_years, d_xrays, width,                label='X-RAY DIFFRACTION' , fill = None   , edgecolor="black")
-embars   = ax.bar(d_years, d_ems  , width, bottom=d_xrays,label='ELECTRON MICROSCOPY'  , color="black", edgecolor="black")
+xraybars = ax.bar(d_years, d_xrays, width,                label='X-RAY DIFFRACTION' , color="black"   , edgecolor="black")
+embars   = ax.bar(d_years, d_ems  , width, bottom=d_xrays,label='ELECTRON MICROSCOPY'  ,fill=None , edgecolor="black")
 plt.xticks(fontsize=18)
 plt.yticks(fontsize=18)
 # ax.set_ylabel('Scores')
@@ -96,7 +93,7 @@ plt.legend(loc=2, prop={'size': 18})
 
 
 # ax.set_xticklabels(['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'])
-        
+
 
 for ( bar, i_em, i_xray ) in zip(embars.patches, d_ems, d_xrays):
     value = bar.get_height()
@@ -106,7 +103,7 @@ for ( bar, i_em, i_xray ) in zip(embars.patches, d_ems, d_xrays):
     ax.text(text_x, text_y + 3, text, ha='center',color='black',size=18)
 
 fig.set_size_inches(18.5, 10.5)
-# plt.show()
-fig.savefig('cumulative_entries.png', format='png', dpi=300, bbox_inches='tight')
-fig.savefig('cumulative_entries.pdf', format='pdf', dpi=300, bbox_inches='tight')
+plt.show()
+# fig.savefig('cumulative_entries.png', format='png', dpi=300, bbox_inches='tight')
+# fig.savefig('cumulative_entries.pdf', format='pdf', dpi=300, bbox_inches='tight')
 
