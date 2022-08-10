eb22b808aca25a5ba4729382a94f61107b6e84a3
Artem <rtkushner@gmail.com>
2022-08-10 09:28:58 -0700

    years dict

--- ribetl/stats/reso_by_species.py
+++ ribetl/stats/reso_by_species.py
@@ -23,7 +23,7 @@ profiles      = list(map(lambda _: os.path.join(STATIC_ROOT,_,f"{_}.json"),struc
 
 
 species_dict = {}
-
+years_dict = {}
 
 
 for path in profiles:
@@ -32,10 +32,18 @@ for path in profiles:
         src_ids = profile['src_organism_ids']
         if len(src_ids)<1:continue
         
-        organism = src_ids[0]
-        reso     = profile['resolution']
-        method   = profile['expMethod']
+        organism     = src_ids[0]
+        reso         = profile['resolution']
+        method       = profile['expMethod']
+        release_date = profile['citation_year']
+        if release_date == None:continue
+        if release_date not in years_dict:
+            years_dict[release_date] =[organism]
+        else:
+            years_dict[release_date] =[organism, *years_dict[release_date]]
 
+
+        print(release_date)
         if organism not in species_dict:
             species_dict[organism] = {
                 'X-RAY DIFFRACTION'  : [],
@@ -47,39 +55,62 @@ for path in profiles:
 
         species_dict[organism][method].append(reso)
 
+# -------------------------------------------------------------------------------- YEARS DICT
+sorted_by_year = sorted(years_dict.items(), key=lambda x: x[0])
 
-562 
-83333
+last10years    = sorted_by_year[-10:]
+all            = sorted_by_year
 
-species_dict[562] ={
-    'count'               :   species_dict[562]['count'              ] + species_dict[83333]['count'               ],
-    'X-RAY DIFFRACTION'   : [*species_dict[562]['X-RAY DIFFRACTION'  ], *species_dict[83333]['X-RAY DIFFRACTION'  ]],
-    'ELECTRON MICROSCOPY' :[* species_dict[562]['ELECTRON MICROSCOPY'], *species_dict[83333]['ELECTRON MICROSCOPY']], 
-}
+allset = set();
+for x in all:
+    allset.update(x[1])
 
-del(species_dict[83333])
+last10yearsset = set();
+for i in last10years:
+    last10yearsset.update(i[1])
 
-sorted_by_count = sorted(species_dict.items(), key=lambda x: x[1]['count'])
-for item in sorted_by_count[-5:]:
-    assert(len(item[1]['X-RAY DIFFRACTION']) +len(item[1]['ELECTRON MICROSCOPY']) == item[1]['count']) 
-    print("Count:",item[1]['count'])
-    print("Species: ", ncbi.get_taxid_translator([ item[0] ]))
-    print("Avg xray",  np.mean(item[1]['X-RAY DIFFRACTION']))
-    print("Avg em",  np.mean(item[1]['ELECTRON MICROSCOPY']))
-    print("\n")
+print(len(allset))
+print(len(last10yearsset))
 
 
-rest_xrays = []
-rest_ems   = []
-rest_count = 0
 
-for rest_item in sorted_by_count[:-5]:
-    rest_count +=              rest_item[1]['count'               ]
-    rest_xrays =[*rest_xrays, *rest_item[1]['X-RAY DIFFRACTION'  ]]
-    rest_ems   =[*rest_xrays, *rest_item[1]['ELECTRON MICROSCOPY']]
+# -------------------------------------------------------------------------------- YEARS DICT
+########
+#######
+######
+####
+##
+#
+
+# species_dict[562] ={
+#     'count'               :   species_dict[562]['count'              ] + species_dict[83333]['count'               ],
+#     'X-RAY DIFFRACTION'   : [*species_dict[562]['X-RAY DIFFRACTION'  ], *species_dict[83333]['X-RAY DIFFRACTION'  ]],
+#     'ELECTRON MICROSCOPY' :[* species_dict[562]['ELECTRON MICROSCOPY'], *species_dict[83333]['ELECTRON MICROSCOPY']], 
+# }
+
+# del(species_dict[83333])
+
+# sorted_by_count = sorted(species_dict.items(), key=lambda x: x[1]['count'])
+# for item in sorted_by_count[-5:]:
+#     assert(len(item[1]['X-RAY DIFFRACTION']) +len(item[1]['ELECTRON MICROSCOPY']) == item[1]['count']) 
+#     print("Count:",item[1]['count'])
+#     print("Species: ", ncbi.get_taxid_translator([ item[0] ]))
+#     print(f"Avg xray({len(item[1]['X-RAY DIFFRACTION'])})",  np.mean(item[1]['X-RAY DIFFRACTION']))
+#     print(f"Avg em({len(item[1]['ELECTRON MICROSCOPY'])})",  np.mean(item[1]['ELECTRON MICROSCOPY']))
+#     print("\n")
 
 
-print("\n")
-print("rest Count:", rest_count)
-print("rest Avg xray",  np.mean(rest_xrays))
-print("rest Avg em",  np.mean(rest_ems))
+# rest_xrays = []
+# rest_ems   = []
+# rest_count = 0
+
+# for rest_item in sorted_by_count[:-5]:
+#     rest_count +=              rest_item[1]['count'               ]
+#     rest_xrays =[*rest_xrays, *rest_item[1]['X-RAY DIFFRACTION'  ]]
+#     rest_ems   =[*rest_ems, *rest_item[1]['ELECTRON MICROSCOPY']]
+
+
+# print("\n")
+# print("rest Count:", rest_count)
+# print(f"rest Avg xray({len(rest_xrays)})",  np.mean(rest_xrays))
+# print(f"rest Avg em({len(rest_ems)})",  np.mean(rest_ems))
